7aaca5b3a72b86751351afed93360d67d8f55ab9
Serge <sergiusz.cichosz@gmail.com>
2023-03-25 01:16:50 +0000

    Fix imports

--- src/jsMain/kotlin/Apistuff.kt
+++ src/jsMain/kotlin/Apistuff.kt
@@ -1,9 +1,8 @@
-import io.ktor.client.*
-import io.ktor.client.call.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.request.*
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
 
 external fun getBaseUrl(): String
 val baseUrl = getBaseUrl()
