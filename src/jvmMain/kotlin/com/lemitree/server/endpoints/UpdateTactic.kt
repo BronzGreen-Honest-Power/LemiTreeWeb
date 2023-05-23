372f5aa1d408cc58cf3cf41b1d38737a160f9a13
Serge <sergiusz.cichosz@gmail.com>
2023-05-23 01:19:14 +0100

    Refactor: Extract PR logic

--- src/jvmMain/kotlin/com/lemitree/server/endpoints/UpdateTactic.kt
+++ src/jvmMain/kotlin/com/lemitree/server/endpoints/UpdateTactic.kt
@@ -5,7 +5,6 @@ import com.lemitree.common.data.toMarkdown
 import com.lemitree.common.helpers.getKoinInstance
 import com.lemitree.server.BASE_DIR
 import com.lemitree.server.helpers.ioLaunch
-import com.lemitree.server.helpers.runCommand
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.ApplicationCall
 import io.ktor.server.application.call
@@ -13,14 +12,13 @@ import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.util.pipeline.PipelineContext
 import java.io.File
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 suspend fun PipelineContext<Unit, ApplicationCall>.processNewTactic(
     baseDir: String = getKoinInstance(BASE_DIR)
-) {
-    processEditTactic(baseDir = baseDir, editingAllowed = false)
-}
+) = processEditTactic(
+    baseDir = baseDir,
+    editingAllowed = false
+)
 
 /**
  * Create file with specification
@@ -34,57 +32,35 @@ suspend fun PipelineContext<Unit, ApplicationCall>.processNewTactic(
 suspend fun PipelineContext<Unit, ApplicationCall>.processEditTactic(
     baseDir: String = getKoinInstance(BASE_DIR),
     editingAllowed: Boolean = true,
-) {
-    ioLaunch {
-        val tactic = call.receive<Tactic>()
-        println("Received tactic:\n$tactic")
-        val fileContent = tactic.toMarkdown()
-        val fileName = tactic.fileName
-        val filePath = "${baseDir}/${tactic.path}/$fileName"
-        val newFile = File(filePath)
-        if (newFile.exists() && !editingAllowed) {
-            call.respond(HttpStatusCode.Conflict, "Tactic with this name already exists.")
-        } else {
-            withContext(Dispatchers.IO) {
-                println("Creating file: $filePath")
-                newFile.createNewFile().also {
-                    if (it) {
-                        println("Writing content of $filePath:")
-                        println(fileContent)
-                        newFile.writeText(fileContent)
-                    }
-                }
+) = ioLaunch {
+    val tactic = call.receive<Tactic>()
+    println("Received tactic:\n${tactic}")
+    val fileContent = tactic.toMarkdown()
+    val fileName = tactic.fileName
+    val filePath = "${baseDir}/${tactic.path}/$fileName"
+    val newFile = File(filePath)
+    if (newFile.exists() && !editingAllowed)
+        call.respond(HttpStatusCode.Conflict, "Tactic with this name already exists.")
+    else {
+        println("Creating file: $filePath")
+        @Suppress("BlockingMethodInNonBlockingContext") // Call is already surrounded by ioLaunch
+        newFile.createNewFile().also {
+            if (it) {
+                println("Writing content of $filePath:")
+                println(fileContent)
+                newFile.writeText(fileContent)
             }
         }
-        if (newFile.exists()) {
-            val commitMessage = "New Tactic: $fileName"
-            val branchName = "New_Tactic-${tactic.tacticName}"
-            // todo: test process against potential concurrency issues
-            val successful = File(baseDir).runCommand<Boolean>(
-                // todo: this command is a bit long, consider extracting it to a proper script
-                "git checkout master && " +
-                "git checkout -b $branchName && " +
-                "git add -A && " +
-                "git commit -m \"$commitMessage\" && " +
-                "git push --set-upstream origin $branchName && " +
-                "gh api --method POST " + // todo: this method will return 0 even if unsuccessful, validate response
-                    "-H \"Accept: application/vnd.github+json\" " +
-                    "-H \"X-GitHub-Api-Version: 2022-11-28\" " +
-                    "/repos/BronzGreen-Honest-Power/LemiTree/pulls " +
-                    "-f title=\"$commitMessage\" " +
-                    "-f body=\"$commitMessage\" " + //todo make this something more meaningful perhaps
-                    "-f head=\"$branchName\" " +
-                    "-f base=\"master\" && " +
-                "(git checkout master; echo true) || " +
-                // todo: change stashing to resetting once the system is tested
-                "(git add -A && git stash; git checkout master; echo false)"
-            )
-            if (successful == false) {
-                call.respond(HttpStatusCode.InternalServerError, "Failed to push file to Github.")
-            }
-        } else call.respond(HttpStatusCode.InternalServerError, "Failed to create the tactic file.")
-        call.respond(fileContent)
     }
+    if (newFile.exists())
+        createPullRequest(
+            baseDir = baseDir,
+            branchName = "New_Tactic-${tactic.path}/$fileName",
+            commitMessage = "New Tactic: $fileName",
+            prTitle = "New Tactic: ${tactic.tacticName}}",
+        ) || prCreationFailure()
+    else call.respond(HttpStatusCode.InternalServerError, "Failed to create the tactic file.")
+    call.respond(HttpStatusCode.Created)
 }
 
 //gh api \
