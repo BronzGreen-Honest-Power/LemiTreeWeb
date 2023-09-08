0de027ce7e8a8953de1528de1f18756cc9d6781f
Serge <sergiusz.cichosz@gmail.com>
2023-09-08 23:24:35 +0100

    Fix dropdowns

--- src/jsMain/kotlin/com.lemitree.web/ui/components/SimpleDropdownMenu.kt
+++ src/jsMain/kotlin/com.lemitree.web/ui/components/SimpleDropdownMenu.kt
@@ -3,13 +3,20 @@ package com.lemitree.web.ui.components
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.zIndex
 
 @Composable
 fun SimpleDropdownMenu(
@@ -22,6 +29,11 @@ fun SimpleDropdownMenu(
 ) {
     var expanded by remember { mutableStateOf(false) }
     Column {
+        val textStyle = if (selected != null) LocalTextStyle.current.copy(
+            fontWeight = FontWeight.Black,
+            color = MaterialTheme.colors.primary,
+            fontStyle = FontStyle.Normal,
+        ) else LocalTextStyle.current
         OutlinedTextField(
             value = selected ?: hint,
             onValueChange = {},
@@ -30,12 +42,15 @@ fun SimpleDropdownMenu(
                 expanded = !expanded
                 selected ?: return@clickable
                 onItemSelected(selected)
-            }
+            },
+            enabled = false,
+            textStyle = textStyle,
         )
         if (expanded) {
             Card(
                 modifier = Modifier
                     .wrapContentSize()
+                    .zIndex(2f)
                     .padding(vertical = 2.dp),
                 elevation = 4.dp,
             ) {
@@ -45,8 +60,9 @@ fun SimpleDropdownMenu(
                 ) {
                     items.forEach {
                         Box(
+                            contentAlignment = Alignment.Center,
                             modifier = Modifier
-                                .fillMaxWidth()
+                                .requiredWidth(200.dp)
                                 .padding(4.dp)
                                 .clickable {
                                     expanded = !expanded
