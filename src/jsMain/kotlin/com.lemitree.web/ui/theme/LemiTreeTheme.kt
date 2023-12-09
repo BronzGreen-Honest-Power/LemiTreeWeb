7ea04137bf726c8e7807ce1ac7003d49c8a8770c
Serge <sc9500@proton.me>
2023-12-09 05:18:42 +0000

    Set Arial as default font

--- src/jsMain/kotlin/com.lemitree.web/ui/theme/LemiTreeTheme.kt
+++ src/jsMain/kotlin/com.lemitree.web/ui/theme/LemiTreeTheme.kt
@@ -1,14 +1,33 @@
 package com.lemitree.web.ui.theme
 
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Typography
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.platform.Font
+import org.jetbrains.skiko.loadBytesFromPath
 
 @Composable
 fun LemiTreeTheme(content: @Composable () -> Unit) {
+    var fontFamily by remember { mutableStateOf<FontFamily?>(null) }
+    LaunchedEffect(Unit) {
+        fontFamily = FontFamily(
+            Font(
+                identity = "Arial",
+                data = loadBytesFromPath("arial.ttf")
+            )
+        )
+    }
     MaterialTheme(
         colors = colors(),
+        typography = fontFamily?.let { Typography(defaultFontFamily = it) } ?: MaterialTheme.typography,
         content = content,
     )
 }
